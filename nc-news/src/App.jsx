816e959b6ae840303991eb4ed68bb061a2256991
Chris Spence <christopherspenceuk@outlook.com>
2018-04-05 16:42:53 +0100

    initial setup of application

---
+++ nc-news/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div className="container">
+      <div>Hello World</div>
+    </div>
+  </Router>
+);
+
+export default App;
